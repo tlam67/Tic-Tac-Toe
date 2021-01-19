bba20d9ac6200f901995c4d3966678b26ace3eb2
tlam67 <tlamottawa@gmail.com>
2021-01-18 21:41:18 -0500

    working on pygame display

--- tictactoe.py
+++ tictactoe.py
@@ -12,3 +12,31 @@ import pygame as pg
     # alternate between X and O
     # after each placement, check if player wins
 
+
+#initialize pygame
+pg.init()
+
+
+# drawing window
+display = pg.display.set_mode([500, 500])
+
+
+
+#game loop until quit
+running = True
+while running:
+
+    #if user quits
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    
+    #background colour
+    display.fill((255, 255, 255))
+
+    #update display
+    pg.display.flip()
+
+pg.quit()
+
