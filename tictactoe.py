dfe826a3e55ebfbe50b26f9773122a62c2f934fb
tlam67 <tlamottawa@gmail.com>
2021-01-18 21:34:10 -0500

    initial commit

---
+++ tictactoe.py
@@ -0,0 +1,14 @@
+import pygame as pg
+
+
+
+# function to check if won
+
+# function to check if draw
+
+# pygame allow clickable interface
+
+# game loop:
+    # alternate between X and O
+    # after each placement, check if player wins
+
